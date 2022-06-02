3c57f17f6fedafea6e1879a6755c7ff3470edfc0
mariananunestaborda@gmail.com <mariananunestaborda@gmail.com>
2022-06-02 19:50:07 -0300

    Criando rota do Esqueceu senha

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,20 +2,23 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // Componentes, Contextos, Hooks e Pages
+import { AuthContextProvider } from "./contexts/AuthContext";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { Home } from "./pages/Home";
+import { ForgotPassword } from "./pages/ForgotPassword";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <AuthContextProvider> */}
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/register" exact component={Register} />
-        <Route path="/home" exact component={Home} />
-      </Switch>
-      {/* </AuthContextProvider> */}
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/register" exact component={Register} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/forgot-password" exact component={ForgotPassword} />
+        </Switch>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
